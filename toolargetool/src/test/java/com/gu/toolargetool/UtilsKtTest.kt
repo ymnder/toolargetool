c75e6b2f5a81f55dda603d3325431c1cd72726ad
ymnder <yamazaki.waseda@gmail.com>
2018-08-01 21:22:57 +0900

    add robolectric

--- toolargetool/src/test/java/com/gu/toolargetool/UtilsKtTest.kt
+++ toolargetool/src/test/java/com/gu/toolargetool/UtilsKtTest.kt
@@ -1,10 +1,23 @@
 package com.gu.toolargetool
 
+import android.os.Bundle
+import androidx.test.runner.AndroidJUnit4
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
 
 
+@RunWith(AndroidJUnit4::class)
 class UtilsKtTest {
+
+
+    @Test
+    fun sizeAsParcel() {
+        val bundle = Bundle()
+        bundle.putString("TEST", "t")
+        val parcel = bundle.sizeAsParcel()
+        assertThat(parcel).isEqualTo(25)
+    }
 
     @Test
     fun kb() {
